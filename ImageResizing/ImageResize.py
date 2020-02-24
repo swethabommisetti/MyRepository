a7c2dd4af40750fd4e7d61ba66935bc828c93f6c
swethabommisetti <swethasetti.kandukuri@gmail.com>
2020-02-24 14:55:00 -0500

    -

--- ImageResizing/ImageResize.py
+++ ImageResizing/ImageResize.py
@@ -6,25 +6,25 @@ import ImageResizeParams as irp
 #image1 =cv2.imread("for_blur.png")
 cv2.imshow("first image", irp.image1)
 cv2.waitKey(200)
-r = 150.0 / irp.image1.shape[1]
-dim = (150, int(irp.image1.shape[0] * r))
-resized1 = cv2.resize(irp.image1, dim, interpolation=cv2.INTER_AREA)
-cv2.imshow("Resized (Width)", resized1)
+r = 100.0 / irp.image1.shape[1]
+dim = (100, int(irp.image1.shape[0] * r))
+image1 = cv2.resize(irp.image1, dim, interpolation=cv2.INTER_AREA)
+cv2.imshow("Resized (Width)", image1)
 cv2.waitKey(200)
 
 
 image2 =cv2.imread("for_resizing.png")
 cv2.imshow("second image", irp.image2)
 cv2.waitKey(200)
-r = 150.0 / irp.image2.shape[1]
-dim = (150, int(irp.image2.shape[0] * r))
-resized2 = cv2.resize(irp.image2, dim, interpolation=cv2.INTER_AREA)
-cv2.imshow("Resized (Width)", resized2)
+r = 100.0 / irp.image2.shape[1]
+dim = (100, int(irp.image2.shape[0] * r))
+image2 = cv2.resize(irp.image2, dim, interpolation=cv2.INTER_AREA)
+cv2.imshow("Resized (Width)", image2)
 cv2.waitKey(200)
 
 #creating a collage
 
-collage = np.concatenate((resized1, resized2), axis=0)
+collage = np.concatenate((image1, image2), axis=0)
 cv2.imshow("collage", collage)
 cv2.waitKey(0)
 #trying to put resizing in a loop
